027544f856aa1105db5d7c9ab355c9da34c99b68
wbdinnit <dinnitdinosaur1981@gmail.com>
2025-03-31 21:19:59 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "b33ae428-97b5-4727-8dec-286ad09e452e":
+            case "myImages.image0":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "cda7a490-1a44-4084-5bd1-ffdd17c8b296":
+            case "myImages.image1":return img`
+. . . . c c c c c c . . . . . . 
+. . . c 6 7 7 7 7 6 c . . . . . 
+. . c 7 7 7 7 7 7 7 7 c . . . . 
+. c 6 7 7 7 7 7 7 7 7 6 c . . . 
+. c 7 c 6 6 6 6 c 7 7 7 c . . . 
+. f 7 6 f 6 6 f 6 7 7 7 f . . . 
+. f 7 7 7 7 7 7 7 7 7 7 f . . . 
+. . f 7 7 7 7 6 c 7 7 6 f c . . 
+. . . f c c c c 7 7 6 f 7 7 c . 
+. . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+. c 7 7 2 7 7 c f c 6 7 7 6 c c 
+c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+. f 6 1 1 1 1 1 1 6 6 6 f . . . 
+. . c c c c c c c c c f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,388 @@
+namespace SpriteKind {
+    export const Supply = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile4`, function (sprite, location) {
+    startNextLevel()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -200
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . f 2 f e e e e f f . . . . 
+        . . f 2 2 2 f e e e e f f . . . 
+        . . f e e e e f f e e e f . . . 
+        . f e 2 2 2 2 e e f f f f . . . 
+        . f 2 e f f f f 2 2 2 e f . . . 
+        . f f f e e e f f f f f f f . . 
+        . f e e 4 4 f b e 4 4 e f f . . 
+        . . f e d d f 1 4 d 4 e e f . . 
+        . . . f d d d e e e e e f . . . 
+        . . . f e 4 e d d 4 f . . . . . 
+        . . . f 2 2 e d d e f . . . . . 
+        . . f f 5 5 f e e f f f . . . . 
+        . . f f f f f f f f f f . . . . 
+        . . . f f f . . . f f . . . . . 
+        `)
+})
+function startNextLevel () {
+    info.startCountdown(30)
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        value.destroy()
+    }
+    for (let value of sprites.allOfKind(SpriteKind.Food)) {
+        value.destroy()
+    }
+    currentLevel += 1
+    if (currentLevel == 1) {
+        scene.setBackgroundColor(11)
+        tiles.setTilemap(tilemap`platformer1`)
+    } else if (currentLevel == 2) {
+        scene.setBackgroundColor(11)
+        tiles.setCurrentTilemap(tilemap`level7`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(16, 8))
+    } else if (currentLevel == 3) {
+        scene.setBackgroundColor(11)
+        tiles.setCurrentTilemap(tilemap`level9`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(27, 6))
+    } else if (currentLevel == 4) {
+        scene.setBackgroundColor(11)
+        tiles.setCurrentTilemap(tilemap`level10`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(13, 1))
+    } else if (currentLevel == 5) {
+        tiles.setCurrentTilemap(tilemap`level11`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(25, 1))
+    } else if (currentLevel == 6) {
+        tiles.setCurrentTilemap(tilemap`level12`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(11, 3))
+    } else if (currentLevel == 7) {
+        tiles.setCurrentTilemap(tilemap`level13`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(24, 3))
+    } else if (currentLevel == 8) {
+        tiles.setCurrentTilemap(tilemap`level14`)
+    } else if (currentLevel == 9) {
+        tiles.setCurrentTilemap(tilemap`level15`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(6, 7))
+    } else if (currentLevel == 10) {
+        tiles.setCurrentTilemap(tilemap`level16`)
+        Cheeseburger = sprites.create(img`
+            . . . . . . b b b b a a . . . . 
+            . . . . b b d d d 3 3 3 a a . . 
+            . . . b d d d 3 3 3 3 3 3 a a . 
+            . . b d d 3 3 3 3 3 3 3 3 3 a . 
+            . b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
+            . b 3 3 3 3 3 a a 3 3 3 3 3 a b 
+            b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
+            b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
+            b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
+            a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
+            a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
+            a a 3 3 3 d d d a a 4 4 4 e e . 
+            . e a a a a a a 4 4 4 4 e e . . 
+            . . e e b b 4 4 4 4 b e e . . . 
+            . . . e e e e e e e e . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        grid.place(Cheeseburger, tiles.getTileLocation(11, 6))
+    } else if (currentLevel == 11) {
+        tiles.setCurrentTilemap(tilemap`level17`)
+    } else if (currentLevel == 12) {
+        tiles.setCurrentTilemap(tilemap`level18`)
+    } else {
+        game.splash("KILL SCREEN (error 408)")
+        game.over(true, effects.confetti)
+    }
+    tiles.placeOnRandomTile(mySprite, assets.tile`tile3`)
+    for (let value2 of tiles.getTilesByType(assets.tile`tile5`)) {
+        myEnemy = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f11111111f.......
+            ......fd11111111df......
+            ......fd11111111df......
+            ......fddd1111dddf......
+            ......fbdbfddfbdbf......
+            ......fcdcf11fcdcf......
+            .......fb111111bf.......
+            ......fffcdb1bdffff.....
+            ....fc111cbfbfc111cf....
+            ....f1b1b1ffff1b1b1f....
+            ....fbfbffffffbfbfbf....
+            .........ffffff.........
+            ...........fff..........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(myEnemy, value2)
+        myEnemy.follow(mySprite, 30)
+    }
+}
+info.onCountdownEnd(function () {
+    game.setGameOverMessage(true, "GAME OVER!")
+    game.setGameOverPlayable(true, music.melodyPlayable(music.wawawawaa), false)
+    game.setGameOverScoringType(game.ScoringType.HighScore)
+    game.over(true, effects.dissolve)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . f f e e e e f 2 f . . . . 
+        . . f f e e e e f 2 2 2 f . . . 
+        . . f e e e f f e e e e f . . . 
+        . . f f f f e e 2 2 2 2 e f . . 
+        . . f e 2 2 2 f f f f e 2 f . . 
+        . f f f f f f f e e e f f f . . 
+        . f f e 4 4 e b f 4 4 e e f . . 
+        . f e e 4 d 4 1 f d d e f . . . 
+        . . f e e e e e d d d f . . . . 
+        . . . . f 4 d d e 4 e f . . . . 
+        . . . . f e d d e 2 2 f . . . . 
+        . . . f f f e e f 5 5 f f . . . 
+        . . . f f f f f f f f f f . . . 
+        . . . . f f . . . f f f . . . . 
+        `)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile2`, function (sprite, location) {
+    game.setGameOverMessage(true, "GAME OVER!")
+    game.setGameOverPlayable(true, music.melodyPlayable(music.wawawawaa), false)
+    game.setGameOverScoringType(game.ScoringType.HighScore)
+    game.over(true, effects.dissolve)
+})
+info.onLifeZero(function () {
+    game.setGameOverMessage(true, "GAME OVER!")
+    game.setGameOverPlayable(true, music.melodyPlayable(music.wawawawaa), false)
+    game.setGameOverScoringType(game.ScoringType.HighScore)
+    game.over(true, effects.dissolve)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    sprites.destroy(Cheeseburger)
+    info.changeLifeBy(1)
+    info.changeScoreBy(100)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.ashes, 100)
+    sprites.destroy(sprite, effects.disintegrate, 100)
+    info.changeScoreBy(10)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (sprite.bottom < otherSprite.y) {
+        sprite.vy = -100
+        info.changeScoreBy(10)
+    } else {
+        info.changeLifeBy(-1)
+    }
+    otherSprite.destroy()
+})
+let myEnemy: Sprite = null
+let Cheeseburger: Sprite = null
+let currentLevel = 0
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . f f e e e e f 2 f . . . . 
+    . . f f e e e e f 2 2 2 f . . . 
+    . . f e e e f f e e e e f . . . 
+    . . f f f f e e 2 2 2 2 e f . . 
+    . . f e 2 2 2 f f f f e 2 f . . 
+    . f f f f f f f e e e f f f . . 
+    . f f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 1 f d d e f . . . 
+    . . f e e e e e d d d f . . . . 
+    . . . . f 4 d d e 4 e f . . . . 
+    . . . . f e d d e 2 2 f . . . . 
+    . . . f f f e e f 5 5 f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f . . . f f f . . . . 
+    `, SpriteKind.Player)
+mySprite.ay = 500
+controller.moveSprite(mySprite, 100, 0)
+scene.cameraFollowSprite(mySprite)
+info.setScore(0)
+info.setLife(3)
+startNextLevel()
+game.onUpdate(function () {
+    for (let value of sprites.allOfKind(SpriteKind.Enemy)) {
+        if (value.isHittingTile(CollisionDirection.Bottom)) {
+            if (value.vx < 0 && value.tileKindAt(TileDirection.Left, assets.tile`tile1`)) {
+                value.vy = -150
+            } else if (value.vx > 0 && value.tileKindAt(TileDirection.Right, assets.tile`tile1`)) {
+                value.vy = -150
+            }
+        } else if (value.isHittingTile(CollisionDirection.Left)) {
+            value.vx = 30
+        } else if (value.isHittingTile(CollisionDirection.Right)) {
+            value.vx = -30
+        }
+    }
+})
+game.onUpdateInterval(3000, function () {
+    for (let value2 of tiles.getTilesByType(assets.tile`tile7`)) {
+        myEnemy = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f11111111f.......
+            ......fd11111111df......
+            ......fd11111111df......
+            ......fddd1111dddf......
+            ......fbdbfddfbdbf......
+            ......fcdcf11fcdcf......
+            .......fb111111bf.......
+            ......fffcdb1bdffff.....
+            ....fc111cbfbfc111cf....
+            ....f1b1b1ffff1b1b1f....
+            ....fbfbffffffbfbfbf....
+            .........ffffff.........
+            ...........fff..........
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        tiles.placeOnTile(myEnemy, value2)
+        myEnemy.follow(mySprite, 30)
+    }
+})
